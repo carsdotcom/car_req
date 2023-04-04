632d82e11a970a7d9568e11647966934f2a90295
Christian Koch <54853932+ckoch-cars@users.noreply.github.com>
2023-04-04 09:52:58 -0500

    mix deps

--- mix.exs
+++ mix.exs
@@ -4,12 +4,16 @@ defmodule CarReq.MixProject do
   def project do
     [
       app: :car_req,
+      elixirc_paths: elixirc_paths(Mix.env()),
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/fixtures"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -21,6 +25,14 @@ defmodule CarReq.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # HTTP Client deps
+      {:nimble_options, "~> 0.4"},
+      {:req, "~> 0.3"},
+      {:req_fuse, "~> 0.2"},
+      # telemetry is a transient dependency through req (finch)
+      # but CarReq emits telemetry, so we'll be explicit about it as a dependency.
+      {:telemetry, ">= 0.0.0"}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
